76a7ae6a13e6a701ac0629a3e4dd132ceb058099
srahman14 <s.rahman14@hotmail.com>
2026-04-11 22:53:18 +0100

    created navigation dashboard page

---
+++ frontend/src/app/navigation/page.tsx
@@ -0,0 +1,20 @@
+import NavigationSidebar from "@/components/navigation-sidebar";
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black font-mono">
+      <div className="h-screen w-full flex flex-col md:flex-row">
+        {/* Left Navigation Sidebar */}
+        <NavigationSidebar />
+
+        {/* Right Map Area */}
+        <div className="flex-1 bg-zinc-50 dark:bg-zinc-800 flex items-center justify-center">
+          <p className="text-zinc-500 dark:text-zinc-400">Mapbox on the right</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
