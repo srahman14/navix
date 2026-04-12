eb1ab1c5788afd002c10097371a8049354430425
srahman14 <s.rahman14@hotmail.com>
2026-04-12 23:03:55 +0100

    added map component

--- frontend/src/app/navigation/page.tsx
+++ frontend/src/app/navigation/page.tsx
@@ -1,3 +1,4 @@
+import MapComponent from "@/components/map";
 import NavigationSidebar from "@/components/navigation-sidebar";
 import React from "react";
 
@@ -10,7 +11,8 @@ const page = () => {
 
         {/* Right Map Area */}
         <div className="flex-1 bg-zinc-50 dark:bg-zinc-800 flex items-center justify-center">
-          <p className="text-zinc-500 dark:text-zinc-400">Mapbox on the right</p>
+          <p className="text-zinc-500 dark:text-zinc-400"></p>
+          <MapComponent />
         </div>
       </div>
     </main>
