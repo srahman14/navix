b03e892b0c89e5f453fcedad6d63aa13e69eb2dc
srahman14 <s.rahman14@hotmail.com>
2026-04-22 00:41:55 +0100

    added points type passing to components

--- frontend/src/app/navigation/page.tsx
+++ frontend/src/app/navigation/page.tsx
@@ -2,23 +2,30 @@
 
 import MapComponent from "@/components/map";
 import NavigationSidebar from "@/components/navigation-sidebar";
-import React from "react";
+import React, { useState } from "react";
 
-const page = () => {
+type Point = {
+  id: string;
+  type: "order" | "vehicle";
+  coordinates: [number, number]; // [lng, lat]
+};
+
+const NavigationPage = () => {
+  const [points, setPoints] = useState<Point[]>([]);
   return (
     <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black font-mono">
       <div className="h-screen w-full flex flex-col md:flex-row">
         {/* Left Navigation Sidebar */}
-        <NavigationSidebar />
+        <NavigationSidebar setPoints={setPoints} />
 
         {/* Right Map Area */}
         <div className="flex-1 bg-zinc-50 dark:bg-zinc-800 flex items-center justify-center">
           <p className="text-zinc-500 dark:text-zinc-400"></p>
-          <MapComponent />
+          <MapComponent points={points} />
         </div>
       </div>
     </main>
   );
 };
 
-export default page;
+export default NavigationPage;
