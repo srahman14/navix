af4befe9162193870a741d9abfb2f22e76cabb36
srahman14 <s.rahman14@hotmail.com>
2026-04-23 00:12:19 +0100

    moved from using points to using vehicles and order types

--- frontend/src/app/navigation/page.tsx
+++ frontend/src/app/navigation/page.tsx
@@ -4,24 +4,35 @@ import MapComponent from "@/components/map";
 import NavigationSidebar from "@/components/navigation-sidebar";
 import React, { useState } from "react";
 
-type Point = {
+type Vehicle = {
   id: string;
-  type: "order" | "vehicle";
-  coordinates: [number, number]; // [lng, lat]
+  status: "active" | "idle";
+  orders: number;
+  load: string;
+  startLocation: [number, number]; // [lng, lat]
+  orderId?: string; // Optional order association
+};
+
+type Order = {
+  id: string;
+  priority: "high" | "medium" | "low";
+  weight: string;
+  location: [number, number]; // [lng, lat]
 };
 
 const NavigationPage = () => {
-  const [points, setPoints] = useState<Point[]>([]);
+  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [orders, setOrders] = useState<Order[]>([]);
   return (
     <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black font-mono">
       <div className="h-screen w-full flex flex-col md:flex-row">
         {/* Left Navigation Sidebar */}
-        <NavigationSidebar setPoints={setPoints} />
+        <NavigationSidebar vehicles={vehicles} orders={orders} setVehicles={setVehicles} setOrders={setOrders} />
 
         {/* Right Map Area */}
         <div className="flex-1 bg-zinc-50 dark:bg-zinc-800 flex items-center justify-center">
           <p className="text-zinc-500 dark:text-zinc-400"></p>
-          <MapComponent points={points} />
+          <MapComponent vehicles={vehicles} orders={orders} />
         </div>
       </div>
     </main>
