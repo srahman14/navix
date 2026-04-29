c520d82e5e34aff06579b91714f5046e71bbd85e
srahman14 <s.rahman14@hotmail.com>
2026-04-29 22:33:43 +0100

    added route controller with basic getRoute func for testing

---
+++ backend/controllers/route.controller.js
@@ -0,0 +1,33 @@
+import { fetchFromOSR } from "../services/osr.service.js";
+
+export async function getRouteForCar(req, res) {
+  const { coordinates } = req.body;
+
+  // Validate coordinates
+  if (!coordinates || coordinates.length < 2) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid coordinates",
+    });
+  }
+
+  try {
+    const data = await fetchFromOSR(
+      `https://api.openrouteservice.org/v2/directions/driving-car`,
+      coordinates,
+    );
+
+    res.status(200).json({ 
+        success: true, 
+        content: data 
+    });
+
+  } catch (error) {
+    console.error("Route error: " + error.message);
+
+    res.status(500).json({ 
+        success: false, 
+        message: error.message 
+    });
+  }
+}
