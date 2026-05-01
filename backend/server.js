1e070fbd601709dfa57692d8258b207829d083c9
srahman14 <s.rahman14@hotmail.com>
2026-05-01 23:27:01 +0100

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,32 @@
-import express from "express"; 
+import express from "express";
+import cors from "cors";
 
-// Routes 
-import route from "./routes/route.route.js"
+// Routes
+import route from "./routes/route.route.js";
 const app = express();
-const port = 8080; 
+const port = 8080;
 
 // Middleware
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server running");
+  res.send("Server running");
 });
 
-// POST /api/route 
+// POST /api/route
 // Input: coordinates [lng, lat]
 // Returns: route (geometry)
-// Frontend displays the route on the map 
-app.use("/api/v1/", route)
+// Frontend displays the route on the map
+app.use("/api/v1/", route);
 
 // POST /api/route/{orderID}
-// Input: orderID -> checks if we have the route 
+// Input: orderID -> checks if we have the route
 // for that order already if not, generates the route
 // based on the orderID coordinates
-// Returns: the route for that orderID 
-// Frontend displays the route for that orderID on map 
-
+// Returns: the route for that orderID
+// Frontend displays the route for that orderID on map
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
