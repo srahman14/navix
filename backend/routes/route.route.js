bbd38db2994cfa446fe0e4e995c3a3187d7b1371
srahman14 <s.rahman14@hotmail.com>
2026-05-30 23:16:46 +0100

    add: added route for metrics

--- backend/routes/route.route.js
+++ backend/routes/route.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getRouteForCar } from "../controllers/route.controller.js";
+import { getMetricsFromRoute } from "../services/routeEvaluator.service.js";
 
 const router = express.Router();
 
 router.post("/route", getRouteForCar);
+router.post("/metrics", getMetricsFromRoute)
 
 export default router;
