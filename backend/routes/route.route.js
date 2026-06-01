ebe7066ec4ba8aad094f35f5465fdc658a5bfb2a
srahman14 <s.rahman14@hotmail.com>
2026-06-01 22:28:58 +0100

    add: new routes

--- backend/routes/route.route.js
+++ backend/routes/route.route.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { getMetrics } from "../controllers/evaluator.controller.js";
 import { getRouteForCar } from "../controllers/route.controller.js";
-import { getMetricsFromRoute } from "../services/routeEvaluator.service.js";
+import { getLocationDetails } from "../controllers/reverseGeocode.controller.js";
 
 const router = express.Router();
 
 router.post("/route", getRouteForCar);
-router.post("/metrics", getMetricsFromRoute)
+router.post("/metrics", getMetrics)
+router.post("/geocode", getLocationDetails)
 
 export default router;
