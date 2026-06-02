644ee3fae2f247d6be315993f254d48cfd11e378
srahman14 <s.rahman14@hotmail.com>
2026-06-02 19:33:52 +0100

    add: added getLocation function to call backend reverseGeocode service

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,11 +1,41 @@
 import React from "react";
 
-export const getRoute = async (coordinates: any) => {
+export const getRoute = async (coordinates: any, profile: string = "driving-car") => {
   if (!coordinates || coordinates.length < 2) {
     throw new Error("Invalid coordinates");
   }
 
   const response = await fetch("http://localhost:8080/api/v1/route", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      coordinates: coordinates,
+      profile: profile,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    console.error("API Error: " + data.message)
+    throw new Error(data.message || "Failed to fetch route");
+  }
+
+  if (!data.content?.routes.length) {
+    throw new Error("No routes returned from backend");
+  }
+
+  return data.content;
+};
+
+export const getLocation = async (coordinates: any) => {
+  if (!coordinates || coordinates.length < 2) {
+    throw new Error("Invalid coordinates");
+  }
+
+  const response = await fetch("http://localhost:8080/api/v1/geocode", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,11 +49,7 @@ export const getRoute = async (coordinates: any) => {
 
   if (!response.ok) {
     console.error("API Error: " + data.message)
-    throw new Error(data.message || "Failed to fetch route");
-  }
-
-  if (!data.content?.routes.length) {
-    throw new Error("No routes returned from backend");
+    throw new Error(data.message || "Failed to fetch location");
   }
 
   return data.content;
