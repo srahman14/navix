782dfaa093c62c44fa5e839438a2b590d0669830
srahman14 <s.rahman14@hotmail.com>
2026-06-03 17:53:10 +0100

    WIP: added normalization and built the WSM

--- backend/services/scoringEngine.service.js
+++ backend/services/scoringEngine.service.js
@@ -1,27 +1,47 @@
-// TODO: 
-// - add normalization - normalize distances & durations first 
-// - compute score using normalized distance & duration 
 export const computeScoreRoutes = async (routes) => {
     // Handle single route for backward compatibility
     const routeArray = Array.isArray(routes) ? routes : [routes];
 
-    for (const route in routes) {
+    for (const route of routes) {
         if (!route.summary || route.summary.distance === undefined || route.summary.duration === undefined) {
             throw new Error("All routes must have summaries with distances and durations")
         } 
     }
 
-    return routeArray.map((route, index) => {
-        // Score of Route
-        const score = 0
-        // Weights 
-        const distanceWeight = 0.5
-        const durationWeight = 0.5
-        // Distance & Duration from Route 
-        const distance = route.summary.distance;
-        const duration = route.summary.duration;
-        
-        score = (distanceWeight * distance) + (durationWeight * duration)
+    // Prepocessing distances and durations 
+    const distances = routeArray.map(r => r.summary.distance)
+    const durations = routeArray.map(r => r.summary.duration)
+
+    // Normalization helper
+    const normalize = (value, min, max) => {
+        if (max === min) return 0;
+        return (value - min) / (max - min);
+    }
+
+    // Min & Max distances/durations
+    const minDistance = Math.min(...distances);
+    const maxDistance = Math.max(...distances);
+    const minDuration = Math.min(...durations);
+    const maxDuration = Math.max(...durations);
+
+    return routeArray.map((route) => {
+        // Normalized distance & duration for current route
+        const normDistance = normalize(route.summary.distance, minDistance, maxDistance);
+        const normDuration = normalize(route.summary.duration, minDuration, maxDuration);
+        // weights for now set to 0.5 (for current v1)
+        // will be configurable for future with user preference, VRP policy, optimization tuning
+        const distanceWeight = 0.5;
+        const durationWeight = 0.5;
+        // Score of Route (WSM)
+        // Lower score = more optimal route 
+        const score = 
+            distanceWeight * normDistance +
+            durationWeight * normDuration;
+
+        return {
+            ...route,
+            score
+        }
         
     });
 }
