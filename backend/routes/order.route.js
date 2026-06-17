17997403a511419a8c8db36624a3e8477161bad7
srahman14 <s.rahman14@hotmail.com>
2026-06-17 21:01:36 +0100

    chore: remove unused services/routes

--- backend/routes/order.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createOrderController, getOrdersController } from "../controllers/order.controller.js";
-
-const router = express.Router(); 
-
-router.post("/", createOrderController)
-router.get("/", getOrdersController)
-
-export default router
--- backend/routes/vehicle.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { createVehicleController, getVehiclesController } from "../controllers/vehicle.controller.js";
-
-const router = express.Router(); 
-
-router.post("/", createVehicleController)
-router.get("/", getVehiclesController)
-
-export default router
